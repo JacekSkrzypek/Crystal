c56d8377d6f07a25582f7383fa8daec4036fdfc5
JacekSkrzypek <jasti05@wp.pl>
2023-02-10 19:32:38 +0100

    Added images-animation in SlideShow.jsx

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,12 +1,51 @@
-import React from 'react';
-import './modules.css';
+import React, { useState, useEffect } from "react";
+import "./modules.css";
 
 const SlideShow = () => {
-    return (
-        <div>
-            <p>slid eshow</p>
+  const images = [
+    "pexels-photo-9558256.jpg",
+    "pexels-photo-9558601.jpg",
+    "pexels-photo-9558791.jpg",
+  ];
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const showSlides = () => {
+    if (currentImage < images.length -1) {
+      setCurrentImage(currentImage + 1);
+    } else {
+      setCurrentImage(0);
+    }
+  };
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      showSlides();
+    }, 2000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  });
+
+  return (
+    <div>
+      <p>slideshow</p>
+
+      <h2>Slideshow</h2>
+
+      <div className="slideshow-container">
+        <div className="mySlides fade">
+          <div className="numbertext"> {currentImage + 1} / {images.length}</div>
+          {images.map((image, id) => {
+                return (
+                    <img src={image} 
+                        className={`image ${currentImage === id && 'image--showed'}`} 
+                        style={{ width: "100px" }} />)
+                })}    
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SlideShow;
