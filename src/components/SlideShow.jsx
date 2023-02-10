e2c548bef20965e5bacd33010cb1ed8d78843ef5
JacekSkrzypek <jasti05@wp.pl>
2023-02-10 15:11:20 +0100

    Added SlideShow.jsx

---
+++ src/components/SlideShow.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './modules.css';
+
+const SlideShow = () => {
+    return (
+        <div>
+            <p>slid eshow</p>
+        </div>
+    );
+};
+
+export default SlideShow;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Products from '../components/ProductsList';
 import SearchEngine from '../components/SearchEngine';
+import SlideShow from '../components/SlideShow';
 import './pages.css'
 
 const main = () => {
     return (
         <>
             <SearchEngine />
+            <SlideShow />
         </>
     );
 };
